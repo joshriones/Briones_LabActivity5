43f79a7b0bb56d22d527af9a987a25c488cca64f
joshriones <joshuabriones2003@gmail.com>
2022-12-16 20:09:24 +0800

    Add exception handling to SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,7 @@ package SimpleCalc;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.InputMismatchException;
 import java.util.Objects;
 
 public class SimpleCalcGUI extends JFrame{
@@ -24,30 +25,41 @@ public class SimpleCalcGUI extends JFrame{
 
     }
     public void calcFunc(){
-        double num1 = Integer.parseInt(tfNumber1.getText());
-        double num2 = Integer.parseInt(tfNumber2.getText());
+        try {
+            if(!tfNumber1.getText().matches("[0-9]+") || !tfNumber2.getText().matches("[0-9]+")){
+                throw new InputMismatchException();
+            }
+            double num1 = Integer.parseInt(tfNumber1.getText());
+            double num2 = Integer.parseInt(tfNumber2.getText());
 
-        String op = (String)cbOperations.getSelectedItem();
+            String op = (String) cbOperations.getSelectedItem();
 
-        switch (Objects.requireNonNull(op)){
-            case "+":
-                double sum = num1 + num2;
-                tfResult.setText((String.format("%.0f",sum)));
-                break;
-            case "-":
-                double diff = num1 - num2;
-                tfResult.setText((String.format("%.0f",diff)));
-                break;
-            case "*":
-                double prod = num1 * num2;
-                tfResult.setText((String.format("%.0f",prod)));
-                break;
-            case "/":
-                double quo = num1 / num2;
-                tfResult.setText((String.format("%.2f", quo)));
-                break;
+            switch (Objects.requireNonNull(op)) {
+                case "+":
+                    double sum = num1 + num2;
+                    tfResult.setText((String.format("%.0f", sum)));
+                    break;
+                case "-":
+                    double diff = num1 - num2;
+                    tfResult.setText((String.format("%.0f", diff)));
+                    break;
+                case "*":
+                    double prod = num1 * num2;
+                    tfResult.setText((String.format("%.0f", prod)));
+                    break;
+                case "/":
+                    double quo = num1 / num2;
+                    if(num2 == 0){
+                        throw new ArithmeticException();
+                    }
+                    tfResult.setText((String.format("%.2f", quo)));
+                    break;
+            }
+        }catch(InputMismatchException e){
+            JOptionPane.showMessageDialog(pnlMain,"Number inputs only", "Error", JOptionPane.ERROR_MESSAGE);
+        }catch(ArithmeticException e){
+            JOptionPane.showMessageDialog(pnlMain,"Can't divide by zero", "Error", JOptionPane.ERROR_MESSAGE);
         }
-
     }
     public static void main(String[] args) {
         SimpleCalcGUI calcApp = new SimpleCalcGUI();
